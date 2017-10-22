494cfb869890871a1681987458cdec89f93cbd4b
Steven Hepting <shepting@gmail.com>
2017-10-22 16:42:19 -0700

    Moved reading logic to Home framework.

--- Sources/Home/AnalogReader.swift
+++ Sources/Home/AnalogReader.swift
@@ -1,24 +1,53 @@
 
 import Foundation
+import SwiftyGPIO
+
+func shortSleep() {
+    usleep(100000)
+}
 
 public struct AnalogReader {
     let dataStore = DataStore()
+    let i2c: SysFSI2C
 
     public init() {
-
+        let i2cs = SwiftyGPIO.hardwareI2Cs(for:.RaspberryPi3)!
+        i2c = i2cs[1] as! SysFSI2C
     }
 
-    public func beginReading() {
-        for _ in 0..<1 {
-            let temp = read()
-            dataStore.saveValue(temp)
-            sleep(1)
-        }
-        dataStore.writeData()
-    }
+    public func readValue() {
+        // sleep(1)
 
-    func read() -> Float {
-        print("Reading")
-        return 72.1
+        // let values: [UInt8] = [0xC3, 0xA3]
+        // let values: [UInt8] = [0xA3, 0xC3]
+        // let values: [UInt8] = [195, 163]
+        // print("195: ", 195.hex()) // c3
+        // print("163: ", 163.hex()) // a3
+
+        // i2cset 1 0x48 0x01 0xc3a3 w
+        // i2cget 1 0x48 0x0 w // 0x3bf5
+        // i2cdump 1 0x48 b
+
+        // Write config data
+
+        // let values = [195, 163]
+        i2c.writeWord(0x48, command: 1, value: (195 << 8 | 163))
+        // i2c.writeData(0x48, command: 1, values: [195, 163])
+        // print("Write: \(i2c.writeData(0x48, command: 1, values:values))")
+        // print("Write: \(i2c.writeWord(0x48, command: 1, value: 0xc3a5))")
+
+        shortSleep()
+        let value = i2c.readWord(0x48, command: 0)
+        // let values = i2c.readData(0x48, command:0)
+        // let high = value & 0xFF
+        // let low = (value >> 8) & 0xFF
+
+        // let value = i2c.readData(0x48, command: 0)
+        // let high: UInt16 = UInt16(value[0]) << 8
+        // let low: UInt16 = UInt16(value[1])
+        // let calculated = Double(high | low) * 4096 / 32768.0
+        // print("Calculated: \(calculated)")
+
+        // print("Read: \(i2c.readWord(0x48, command: 0).hex())")
     }
 }
--- Sources/RaspberrySwift/main.swift
+++ Sources/RaspberrySwift/main.swift
@@ -29,48 +29,11 @@ func shortSleep() {
 }
 
 public func i2c() {
-    let i2cs = SwiftyGPIO.hardwareI2Cs(for:.RaspberryPi3)!
-    let i2c = i2cs[1]
+    let analogReader = AnalogReader()
 
-
-
-    // TODO: set measurement values
     for _ in 1...12 {    
-
-        // sleep(1)
-
-        // let values: [UInt8] = [0xC3, 0xA3]
-        // let values: [UInt8] = [0xA3, 0xC3]
-        // let values: [UInt8] = [195, 163]
-        // print("195: ", 195.hex()) // c3
-        // print("163: ", 163.hex()) // a3
-
-        // i2cset 1 0x48 0x01 0xc3a3 w
-        // i2cget 1 0x48 0x0 w // 0x3bf5
-        // i2cdump 1 0x48 b
-
-        // Write config data
-
-        // let values = [195, 163]
-        i2c.writeWord(0x48, command: 1, value: (195 << 8 | 163))
-        // i2c.writeData(0x48, command: 1, values: [195, 163])
-        // print("Write: \(i2c.writeData(0x48, command: 1, values:values))")
-        // print("Write: \(i2c.writeWord(0x48, command: 1, value: 0xc3a5))")
-
-        shortSleep()
-        let value = i2c.readWord(0x48, command: 0)
-        // let values = i2c.readData(0x48, command:0)
-        // let high = value & 0xFF
-        // let low = (value >> 8) & 0xFF
-
-        // let value = i2c.readData(0x48, command: 0)
-        // let high: UInt16 = UInt16(value[0]) << 8
-        // let low: UInt16 = UInt16(value[1])
-        // let calculated = Double(high | low) * 4096 / 32768.0
-        // print("Calculated: \(calculated)")
-
+        analogReader.readValue()
         sleep(1)
-        // print("Read: \(i2c.readWord(0x48, command: 0).hex())")
     }
 }
 
@@ -87,8 +50,8 @@ func switch_toggle() {
     }
 }
 
-switch_toggle()
-// i2c()
+// switch_toggle()
+i2c()
 
 func main() -> Int {
     print("Hey yo. Hi.")
