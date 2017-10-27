48114dd231c1f0ce8b02606118db1d3f3254e2f5
Steven Hepting <shepting@gmail.com>
2017-10-26 20:54:51 -0700

    Add explanatory comments, and get outputs working for relay as well.

--- examples/directMemory.swift
+++ examples/directMemory.swift
@@ -1,43 +1,64 @@
-//
-// directMemory.swift
-//
+#!/usr/bin/swift
 
-import Glibc
+import Foundation // for String(format, )
+import Glibc // for mmap
 
-func with_mmap() {
+func loopGPIO() {
     let file = open("/dev/mem", O_RDWR | O_SYNC);
-    guard (file >= 0) else {
-        perror("Cannot open path")
-        return
-    }
     defer { close(file) }
 
-    guard let rawPointer = mmap(nil, 1024 * 4, PROT_READ | PROT_WRITE, MAP_SHARED, file, 0x3f200000), rawPointer != UnsafeMutableRawPointer(bitPattern: -1) else {
+    
+    // This is the base memory address for *all* peripherals of the
+    // Rasbperry Pi (I2C, SPI, GPIO, UART, etc)
+    let peripheralsBaseAddress = 0x3f000000 
+
+    // This is the offset of just the GPIO configuration registers
+    // from the base peripheral address
+    let gpioBaseOffset = 0x200000
+
+    // We only really care about the memory from this point
+    let gpioBaseAddress = peripheralsBaseAddress + gpioBaseOffset
+
+    // Open the /dev/mem file (all memory) with read and write
+    // privileges (we want to turn on output pins) at the proper
+    // offset (we don't care about the rest of the addresses)
+    guard let rawPointer = mmap(nil, 1024 * 4, PROT_READ | PROT_WRITE, MAP_SHARED, file, gpioBaseAddress) else {
         perror("Cannot mmap bytes for path")
         return
     }
-    let basePointer: UnsafeMutablePointer<UInt32> = rawPointer.bindMemory(to: UInt32.self, capacity: 1024)
+    // This is a typed pointer to the root of the GPIO configuration 
+    // registers. Pointer math will increment by 4 bytes at a time.
+    let basePointer: UnsafeMutablePointer<UInt32> = rawPointer.bindMemory(to: UInt32.self, capacity: 30)
 
-    basePointer.pointee = 0x1000
-    print("Output type pointer \(basePointer) 0x\(String(basePointer.pointee, radix: 16, uppercase: false))")
+    // Configure GPIO pin 4 & 6 as output (3 bits/pin) 
+    // pin numbers:           6  5  4  3  2  1  0
+    basePointer.pointee = 0b001000001000000000000
 
+    // Set up set/clear pointer objects
+    // Offsets from docs were given in byte-size values
+    // since our pointers are smart and expecting word 
+    // size values we convert
     let setPointer = basePointer + 0x1C/4
-    print("Set pointer: \(setPointer)")
     let clearPointer = basePointer + 0x28/4
+
+    // Print the details for sanity sake
+    print("Configuration Details")
+    print("  address: \(basePointer)")
+    print(String(format: "  format: 0x%X", basePointer.pointee))
+
+    print("Set pointer: \(setPointer)")
     print("Clear pointer: \(clearPointer)")
 
+    // Loop through and show the LED turning on and off
     for i in 0...5 {
         print("High \(setPointer)")
-        bcm2835_peri_write(setPointer, 0x10)
-        // setPointer.pointee = 0x10
+        setPointer.pointee = 0b1010000 // Pin 6 & 4
         usleep(500000)
         print("Counter: \(i)")
         print("Low \(clearPointer)")
-        bcm2835_peri_write(clearPointer, 0x10)
-
-        // clearPointer.pointee = 0x10
+        clearPointer.pointee = 0b1010000 // Pin 6 & 4
         usleep(500000)
     }
 }
 
-with_mmap()
+loopGPIO()
