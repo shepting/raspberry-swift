e5f395ccbb343e572b0635d58172a1f7218b4dcf
Steven Hepting <steven.hepting@airbnb.com>
2017-10-28 09:56:31 -0700

    Add datasheets. Update playground.

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ sudo apt install libcurl3
 Sample Python configuration and bit shifting: https://github.com/switchdoclabs/SDL_Pi_Grove4Ch16BitADC/blob/58e2a8e32b03d2c463bde59821d210b16a792ad1/Adafruit_ADS1x15/Adafruit_ADS1x15.py#L266-L268
 
 # Resources
+ - Umberto Raimondi (uraimo) "SwiftyGPIO" https://github.com/uraimo/SwiftyGPIO
  - Marius Rackwitz "Swift HomeKit Accessories" https://speakerdeck.com/marius/diy-smarthome-build-your-own-homekit-accessories
  - Nate Cook "Swift's Pointy Bits: Unsafe Swift and Pointer Types" https://academy.realm.io/posts/nate-cook-tryswift-tokyo-unsafe-swift-and-pointer-types/
  
--- examples/UnsafeMemory.playground/Contents.swift
+++ examples/UnsafeMemory.playground/Contents.swift
@@ -16,20 +16,6 @@ MemoryLayout<Bool>.size         // returns 1
 MemoryLayout<Bool>.alignment    // returns 1
 MemoryLayout<Bool>.stride       // returns 1
 
-MemoryLayout<Float>.size        // returns 4
-MemoryLayout<Float>.alignment   // returns 4
-MemoryLayout<Float>.stride      // returns 4
-
-MemoryLayout<Double>.size       // returns 8
-MemoryLayout<Double>.alignment  // returns 8
-MemoryLayout<Double>.stride     // returns 8
-
-struct EmptyStruct {}
-
-MemoryLayout<EmptyStruct>.size      // returns 0
-MemoryLayout<EmptyStruct>.alignment // returns 1
-MemoryLayout<EmptyStruct>.stride    // returns 1
-
 struct SampleStruct {
     let number: UInt32
     let flag: Bool
@@ -38,9 +24,6 @@ struct SampleStruct {
 MemoryLayout<SampleStruct>.size       // returns 5
 MemoryLayout<SampleStruct>.alignment  // returns 4
 MemoryLayout<SampleStruct>.stride     // returns 8
-
-print("Hey")
-
 
 // 1
 let count = 2
